fbe1284db00b27bfb97612f04832f31651b4cd60
Eqan Abbas <101990803+EkaanAbbas@users.noreply.github.com>
2022-09-20 13:26:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function onSubmitForm() {
 
   if (
     data.userid == "" &&
-    data.itnumber == "" &&
+    data.name == "" &&
     data.title == "" &&
     data.msg == ""
   ) {
@@ -26,7 +26,7 @@ function readData() {
   var data = {};
 
   data["userid"] = document.getElementById("userid").value;
-  data["itnumber"] = document.getElementById("itnumber").value;
+  data["name"] = document.getElementById("name").value;
   data["title"] = document.getElementById("title").value;
   data["msg"] = document.getElementById("msg").value;
 
@@ -42,7 +42,7 @@ function addData(data) {
   cell1 = newrow.insertCell(0);
   cell1.innerHTML = data.userid;
   cell2 = newrow.insertCell(1);
-  cell2.innerHTML = data.itnumber;
+  cell2.innerHTML = data.name;
   cell3 = newrow.insertCell(2);
   cell3.innerHTML = data.title;
   cell4 = newrow.insertCell(3);
@@ -54,7 +54,7 @@ function addData(data) {
 
 function onResetForm() {
   document.getElementById("userid").value = "";
-  document.getElementById("itnumber").value = "";
+  document.getElementById("name").value = "";
   document.getElementById("title").value = "";
   document.getElementById("msg").value = "";
   counter = null;
@@ -71,14 +71,14 @@ function delform(r) {
 function editform(td) {
   counter = td.parentElement.parentElement;
   document.getElementById("userid").value = counter.cells[0].innerHTML;
-  document.getElementById("itnumber").value = counter.cells[1].innerHTML;
+  document.getElementById("name").value = counter.cells[1].innerHTML;
   document.getElementById("title").value = counter.cells[2].innerHTML;
   document.getElementById("msg").value = counter.cells[3].innerHTML;
 }
 
 function updateform(data) {
   counter.cells[0].innerHTML = data.userid;
-  counter.cells[1].innerHTML = data.itnumber;
+  counter.cells[1].innerHTML = data.name;
   counter.cells[2].innerHTML = data.title;
   counter.cells[3].innerHTML = data.msg;
 }
@@ -94,7 +94,7 @@ fetch("data.json")
     objData.map((values) => {
       tbldata += `<tr>
       <td>${values.userid}</td>
-      <td>${values.itnumber}</td>
+      <td>${values.name}</td>
       <td>${values.title}</td>
       <td>${values.msg}</td>
       <td>
